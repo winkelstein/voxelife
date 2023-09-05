a0db0ca010ad4942aa86803f78e3a60c09617d0c
Andrew Winkelstein <andrew.winkelstein@gmail.com>
2023-09-05 04:06:28 +0300

    feat(engine): add model class

---
+++ src/Engine/Model.cpp
@@ -0,0 +1,20 @@
+#include "Model.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+Engine::Model::Model(std::vector<std::shared_ptr<const Mesh>> meshes) : meshes(meshes)
+{
+    this->model = glm::translate(glm::mat4(1.0), glm::vec3(0.0f, 0.0f, -1.0f));
+}
+
+void Engine::Model::draw(gltk::Shader &shader) const
+{
+    shader.bind();
+    shader.uniform<glm::mat4>("model", this->model);
+    for (auto &mesh : this->meshes)
+        mesh->draw(shader);
+}
+
+void Engine::Model::position(glm::vec3 pos)
+{
+    this->model = glm::translate(glm::mat4(1.0), pos);
+}
---
+++ src/Engine/Model.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include "Mesh.h"
+
+namespace Engine
+{
+    class Model : public drawable
+    {
+    private:
+        std::vector<std::shared_ptr<const Mesh>> meshes;
+
+        glm::mat4 model;
+
+    private:
+        static std::vector<float> import_model(std::filesystem::path path);
+        void assemble(std::vector<float> vertices);
+
+    public:
+        Model(std::vector<std::shared_ptr<const Mesh>> meshes);
+
+    public:
+        void draw(gltk::Shader &shader) const override;
+
+    public:
+        void position(glm::vec3 pos);
+        inline glm::vec3 position() const { return model[3]; }
+
+    public:
+        // TODO: make rotate and other model tranformation things
+    };
+}
