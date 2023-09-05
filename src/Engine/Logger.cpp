57ad55432bfae9ccd95500c9d929479a42a28eec
Andrew Winkelstein <andrew.winkelstein@gmail.com>
2023-09-05 15:05:56 +0300

    fix(logger): divide logger to Logger.h and Logger.cpp & fix time printing

---
+++ src/Engine/Logger.cpp
@@ -0,0 +1,50 @@
+#include "Logger.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#define END "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+
+void Engine::Logger::flush()
+{
+    while (!this->qMessages.empty())
+    {
+        this->out(this->qMessages.front());
+        this->qMessages.pop();
+    }
+}
+
+void Engine::Logger::out(const message &msg)
+{
+    std::tm now = this->currentTime();
+
+    std::lock_guard<std::recursive_mutex> lock(this->mtx);
+    static std::ofstream outputfile("debug.log");
+
+#ifdef _WIN32
+    if (msg.sev == severity::high)
+        MessageBoxA(nullptr, msg.body.c_str(), msg.header.c_str(), MB_ICONERROR);
+#endif
+
+    switch (msg.sev)
+    {
+    case severity::high:
+        std::cout << RED;
+        break;
+    case severity::medium:
+    case severity::low:
+        std::cout << YELLOW;
+        break;
+    case severity::notification:
+        std::cout << GREEN;
+        break;
+    }
+
+    std::cout << "[" << std::setw(2) << std::setfill('0') << now.tm_hour << ":" << std::setw(2) << std::setfill('0') << now.tm_min << ":" << std::setw(2) << std::setfill('0') << now.tm_sec << " " << msg.header << "]: ";
+    std::cout << END;
+    std::cout << msg.body << std::endl;
+    outputfile << "[" << now.tm_hour << ":" << now.tm_min << ":" << now.tm_sec << "] " << msg.header << ": " << msg.body << std::endl;
+}
--- src/Engine/Logger.h
+++ src/Engine/Logger.h
@@ -7,11 +7,6 @@
 #include <atomic>
 #include <ctime>
 
-#define END "\033[0m"
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-
 namespace Engine
 {
     class Logger
@@ -67,45 +62,4 @@ namespace Engine
     protected:
         void out(const message &msg);
     };
-}
-
-void Engine::Logger::flush()
-{
-    while (!this->qMessages.empty())
-    {
-        this->out(this->qMessages.front());
-        this->qMessages.pop();
-    }
-}
-
-void Engine::Logger::out(const message &msg)
-{
-    std::tm now = this->currentTime();
-
-    std::lock_guard<std::recursive_mutex> lock(this->mtx);
-    static std::ofstream outputfile("debug.log");
-
-#ifdef _WIN32
-    if (msg.sev == severity::high)
-        MessageBoxA(nullptr, msg.body.c_str(), msg.header.c_str(), MB_ICONERROR);
-#endif
-
-    switch (msg.sev)
-    {
-    case severity::high:
-        std::cout << RED;
-        break;
-    case severity::medium:
-    case severity::low:
-        std::cout << YELLOW;
-        break;
-    case severity::notification:
-        std::cout << GREEN;
-        break;
-    }
-
-    std::cout << "[" << now.tm_hour << ":" << now.tm_min << ":" << now.tm_sec << " " << msg.header << "]: ";
-    std::cout << END;
-    std::cout << msg.body << std::endl;
-    outputfile << "[" << now.tm_hour << ":" << now.tm_min << ":" << now.tm_sec << "] " << msg.header << ": " << msg.body << std::endl;
 }
